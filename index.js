da29e8f2ff2353cb66b48c8924c0e3eb11434cdb
Rebecca Wollard <rbwollard@gmail.com>
2022-12-13 23:14:52 -0700

    commit tree 348dfefdcf3ea7386157a468ad29da1af497b8d6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul= document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+// element.append(ul);
+
+// const info = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+
+// info.textContent = "You've changed what's on the screen!";
+
+// info.style.fontSize = "24px";
+// info.style.fontWeight = "bold";
+// info.style.color = "black";
+
+const newHeader = document.createElement("h1");
+
+
+const main = document.getElementById("main");
+
+main.remove();
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = "is the champion!"
+
+
+
